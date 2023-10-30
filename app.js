834b62929955b7cbf522ff819fb9f4ee8245a140
valenium <valenium@live.com>
2023-10-30 11:04:25 -0500

    edit css

--- app.js
+++ app.js
@@ -14,10 +14,12 @@ const board =[
 
 console.log(board)
 
-const chewyPhrases =['zzz', '2/10', 'am bored', 'can we go home', 'mediocre at best', 'k', 'feed me', 'wher r treats', 'kinda lame', 'bad', 'Y']
+const chewyPhrases =['zzz', '2/10', 'am bored', 'can we go home', 'mediocre at best', 'k', 'feed me', 'wher r treats', 'kinda lame', 'bad', 'Y', 'congrats']
 
 /*----- state variables -----*/
-
+let turn;
+let p1Score;
+let p2Score;
 
 /*----- cached elements  -----*/
 
@@ -26,11 +28,44 @@ const validTileEl = document.querySelectorAll('.play-tile')
 const nullTileEl = document.querySelectorAll('.invalid-tile')
 
 // Play pieces
-const p1Piece = document.querySelectorAll('.p1-piece')
-const p2Piece = document.querySelectorAll('.p2-piece')
+const p1PieceEl = document.querySelectorAll('.p1-piece')
+const p2PieceEl = document.querySelectorAll('.p2-piece')
 
+// Dialogue/stat box
+const chewyTextEl = document.querySelector('#chewy-dialogue')
 
 /*----- event listeners -----*/
-
+p1PieceEl.forEach(function(move){
+    move.addEventListener('click', handlePieceClick)
+})
 
 /*----- functions -----*/
+
+init()
+
+function init(){
+    p1Score = 12
+    p2Score = 12
+    console.log(chewyPhrases[0])
+    // render()
+}
+
+function render(){
+    
+}
+
+function runGame(){
+    if (continueGame()){
+        
+    }else{
+        endGame()
+    }
+}
+
+function continueGame(){
+
+}
+
+function endGame(){
+
+}
